490e357069e536a1afd0da03fdd2e4f8b4d4425e
世外桃源 <ooxfordck@gmail.com>
2016-07-28 16:56:35 +0800

    not working.

--- common/models/reviewer.js
+++ common/models/reviewer.js
@@ -5,4 +5,29 @@
 
 module.exports = function(Reviewer) {
 
+/*
+  Reviewer.disableRemoteMethod("create", true);
+  Reviewer.disableRemoteMethod("upsert", true);
+  Reviewer.disableRemoteMethod("updateAll", true);
+  Reviewer.disableRemoteMethod("updateAttributes", false);
+
+  Reviewer.disableRemoteMethod("find", true);
+  Reviewer.disableRemoteMethod("findById", true);
+  Reviewer.disableRemoteMethod("findOne", true);
+
+  Reviewer.disableRemoteMethod("deleteById", true);
+
+  Reviewer.disableRemoteMethod("confirm", true);
+  Reviewer.disableRemoteMethod("count", true);
+  Reviewer.disableRemoteMethod("exists", true);
+  Reviewer.disableRemoteMethod("resetPassword", true);
+
+  Reviewer.disableRemoteMethod('__count__accessTokens', false);
+  Reviewer.disableRemoteMethod('__create__accessTokens', false);
+  Reviewer.disableRemoteMethod('__delete__accessTokens', false);
+  Reviewer.disableRemoteMethod('__destroyById__accessTokens', false);
+  Reviewer.disableRemoteMethod('__findById__accessTokens', false);
+  Reviewer.disableRemoteMethod('__get__accessTokens', false);
+  Reviewer.disableRemoteMethod('__updateById__accessTokens', false);*/
+
 };
