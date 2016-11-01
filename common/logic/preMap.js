a2d65b8f12c85e14f51e637ac56579b192233185
hesby <jobhesk@gmail.com>
2016-11-01 17:07:29 +0800

    add daily edit
    done

---
+++ common/logic/preMap.js
@@ -0,0 +1,152 @@
+/**
+ * Created by hesk on 16年10月11日.
+ */
+const
+  tool_crypt = require('crypto'),
+  numCPUs = require('os').cpus.length,
+  path = require('path'),
+  uploaderMU = require('multer'),
+  mapSliceArc = require('mapslice'),
+  output = require('.././util/outputjson.js'),
+  rmdir = require('.././util/rmdir.js'),
+  _ = require('lodash'),
+  async = require('async'),
+  fse = require('fs-extra'),
+  _moduleIm = require('zyn-imagemagick'),
+  s3thread = require('./s3upload.js'),
+  colorPaletteGenerator = require('colors-palette'),
+  basemapInfo = require('./basemapinfo.js')
+  ;
+
+const
+  logTag = "> preMap.js",
+  __parentDir = path.dirname(module.main),
+  saltFile = "cath52i43#$#^ebs*^%$ig69",
+  upload_file_field = "art",
+  upload_helper_folder = __parentDir + "/storage/tmp/tmpsgi/",
+  base_folder = __parentDir + "/storage/tmp/storage_f/",
+  base_folder_process = __parentDir + "/storage/tmp/tmpsgi/",
+  public_folder_path = "/static/storage_f/"
+
+  ;
+
+
+var setupUploader = function (_data_pre, extraOperationFromAfterNameDefined, callback_err) {
+  var _baseFolderName = "";
+  var _storage = uploaderMU.diskStorage({
+    destination: function (req, file, cb) {
+      console.log(logTag, '==================================');
+      console.log(logTag, 'check folder structure and define upload folder structures');
+      console.log(logTag, '==================================');
+
+      _baseFolderName = 'basemap-' + Date.now();
+      _data_pre.folder_path = public_folder_path + _baseFolderName + "/";
+      var folder_tmp = base_folder + _baseFolderName;
+      fse.mkdirs(folder_tmp, function (err) {
+        if (_.isError(err)) {
+          return callback_err(err);
+        } else {
+          fse.mkdirsSync(base_folder_process);
+          console.log(logTag, '==================================');
+          console.log(logTag, 'create folder that is not existed!');
+          console.log(logTag, folder_tmp);
+          console.log(logTag, base_folder_process);
+          console.log(logTag, '==================================');
+          cb(null, folder_tmp);
+        }
+      });
+    },
+    filename: function (req, file, cb) {
+      console.log(logTag, "rename file");
+      var hash = tool_crypt.createHmac('sha256', saltFile)
+        .update(_baseFolderName)
+        .digest('hex');
+
+      _data_pre.secret_base_map_file = hash.substring(0, 18) + '.jpg';
+      _data_pre.rename_file = _baseFolderName + '.jpg';
+      _data_pre.mid_size = _baseFolderName + '_mid.jpg';
+      _data_pre.folder_base_name = _baseFolderName;
+
+      if (_.isFunction(extraOperationFromAfterNameDefined)) {
+        extraOperationFromAfterNameDefined(_data_pre);
+      }
+
+      cb(null, _data_pre.secret_base_map_file);
+    }
+  });
+
+
+  return uploaderMU({
+    fileFilter: function fileFilter(req, file, cb) {
+      // The function should call `cb` with a boolean
+      // to indicate if the file should be accepted
+      // To reject this file pass `false`, like so:
+      // cb(null, false)
+      // To accept the file pass `true`, like so:
+      // cb(null, true)
+      // You can always pass an error if something goes wrong:
+      console.log(logTag, file.mimetype);
+      var okay_format = 'image/jpeg';
+      if (file.mimetype == okay_format) {
+        cb(null, true);
+      } else {
+        var str = 'Cannot accept this upload. I don\'t have a clue!';
+        console.log(logTag, str);
+        cb(new Error(str));
+      }
+    },
+    storage: _storage,
+    //dest: __parentDir + '/tmp/',
+    limits: {fileSize: 104857600, files: 1}
+  }).single(upload_file_field);
+};
+
+/*
+ colorPaletteGenerator("/path/to/img", 8, function(err, colors){
+ if(err){
+ console.error(err);
+ return false;
+ }
+ console.log(colors);
+ });
+ */
+module.exports = {
+  logTag: logTag,
+  __parentDir: path.dirname(module.main),
+  saltFile: 'catherineboobsarebig69',
+  upload_file_field: 'art',
+  base_folder: base_folder,
+  base_folder_process: base_folder_process,
+  public_folder_path: public_folder_path,
+  updatebasicconfig: {
+    tileSize: 256,
+    // (default: 256) Tile-size to be used
+    imageMagick: false,
+    // (default: false) If (true) then use ImageMagick instead of GraphicsMagick
+    background: "#FFFFFFFF",
+    // (default: '#FFFFFFFF') Background color to be used for the tiles. More: http://ow.ly/rsluD
+    // tmp: "./tmp",
+    tmp: upload_helper_folder,
+    // (default: '.tmp') Temporary directory to be used to store helper files
+    parallelLimit: 1,
+    // (default: 5) Maximum parallel tasks to be run at the same time (warning: processes can consume a lot of memory!)
+    minWidth: 900,
+    // See explanation about Size detection below
+    skipEmptyTiles: true,
+    // Skips all the empty tiles
+    zoomMin: 1
+  },
+  _: _,
+  numCPUs: numCPUs,
+  mapSliceArc: mapSliceArc,
+  output: output,
+  rmdir: rmdir,
+  async: async,
+  fse: fse,
+  imageMagic: _moduleIm,
+  s3thread: s3thread,
+  basemapInfo: basemapInfo,
+  setupUploader: setupUploader,
+  colorPalGen: colorPaletteGenerator
+
+};
---
+++ common/logic/s3upload.js
@@ -0,0 +1,135 @@
+/**
+ * Created by zJJ on 7/21/2016.
+ */
+const pre = require("./preS3");
+const logTag = "---> transS3";
+const update_meta_on_complete = {
+  "folder_path": "",
+  "listing.enabled": true,
+  "complete": 100
+};
+const setRemoteParamsSample = function (key) {
+  return {
+    params: {
+      Bucket: 'xboxdoc',
+      Key: key,
+      ContentType: 'image/jpeg',
+      ACL: 'public-read'
+    }
+  };
+};
+
+var worker_transfer = function (instance_model, _id, bns) {
+  console.log(logTag, "before process: ", bns);
+  if (!pre.check_access_keys_filled()) {
+    return;
+  }
+  var obfiles = [];
+  if (pre.l.isArray(bns.total_zoom_levels)) {
+    if (bns.total_zoom_levels.length > 0) {
+      pre.l.forEach(bns.total_zoom_levels, function (instance) {
+        var _level = instance.level;
+        console.log(logTag, "level found: " + _level);
+        pre.l.forEach(instance.tiles, function (tile) {
+          var filepath = pre.resolve_format(_level, tile.y, tile.x);
+          obfiles.push(set_aws_worker(bns.folder_base_name + filepath));
+          console.log(logTag, "file added: " + filepath);
+        });
+      });
+      obfiles.push(set_aws_worker(bns.folder_base_name + "/" + bns.rename_file));
+      obfiles.push(set_aws_worker(bns.folder_base_name + "/" + bns.secret_base_map_file));
+      obfiles.push(set_aws_worker(bns.folder_base_name + "/" + bns.mid_size));
+    }
+
+    console.log(logTag, "CPU found:" + numCPUs);
+    triggerS3(obfiles, 0, function () {
+      //When all the S3 files are uploaded.
+      console.log(logTag, "trigger database update.");
+      pre.db.updateByIdUpdate(instance_model, _id, update_meta_on_complete, null);
+    });
+  } else {
+    console.log(logTag, "error cant find it");
+    console.log(logTag, bns);
+  }
+};
+var worker_transfer_simple = function (instance_model, lb_user, basemap_ID, bns, next) {
+  console.log(logTag, "simple transfer process: ", bns);
+  if (!pre.check_access_keys_filled()) {
+    return;
+  }
+  var obfiles = [];
+  obfiles.push(set_aws_worker(bns.folder_base_name + "/" + bns.rename_file));
+  obfiles.push(set_aws_worker(bns.folder_base_name + "/" + bns.secret_base_map_file));
+  obfiles.push(set_aws_worker(bns.folder_base_name + "/" + bns.mid_size));
+  console.log(logTag, "CPU found:" + pre.numCPUs);
+  triggerS3(obfiles, 0, function () {
+    //When all the S3 files are uploaded.
+    console.log(logTag, "trigger database update.");
+    pre.db.updateByIdUpdate(instance_model, basemap_ID, update_meta_on_complete, function (doc) {
+
+      if (pre.l.isError(doc)) {
+        return;
+      }
+
+      pre.db.updateByIdAndIncrease(lb_user, doc["owner"], "uploads", null);
+      if (pre.l.isFunction(next)) {
+        return next();
+      }
+    });
+  });
+};
+
+var triggerS3 = function (tasks, processors, next) {
+  console.log(logTag, "S3 process start");
+  if (processors > 0) {
+    async.parallelLimit(tasks, processors, function (err, results) {
+      console.log(logTag, "S3 process done");
+      next();
+    });
+  } else {
+    async.parallel(tasks, function (err, results) {
+      console.log(logTag, "S3 process done");
+      next();
+    });
+  }
+};
+
+
+var set_aws_worker = function (path_key) {
+  const config = {
+    localFile: pre.fnGetLocalPath(path_key),
+    s3Params: {
+      Bucket: pre.bucket_name,
+      Key: pre.fnGetRemotePath(path_key),
+      ContentType: 'image/jpeg',
+      ACL: 'public-read'
+    }
+  };
+
+  const uploadr = pre.s3_node_client.uploadFile(config);
+  // Upload the file to S3
+  return function worker(callback_aysnc) {
+    uploadr.on('error', function (err) {
+      console.error("unable to upload:", err.stack);
+      return callback_aysnc(err);
+    });
+    uploadr.on('progress', function () {
+      //console.log(logTag, uploadr.progressMd5Amount, uploadr.progressAmount, uploadr.progressTotal);
+    });
+    uploadr.on('end', function () {
+      console.log(logTag, "done uploading: " + path_key);
+      callback_aysnc();
+    });
+  };
+};
+module.exports = {
+  transferSimpleSingleSmallMapS3: worker_transfer_simple,
+  transferSyncBaseMapS3: worker_transfer
+};
+/*
+ var S = require("string"),
+ path = require("path");
+ module.exports.outputresolve = function outputResolve(format, z, y, x) {
+ return S(format).template({z: z, y: y, x: x, google: path.join(String(z), String(y), String(x))}, '{', '}').s;
+ };
+ */
