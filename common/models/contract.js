613469a9f30c8ca12500486b21e21ba9b78977fa
jhes <ooxfordck@gmail.com>
2017-04-03 11:25:58 +0800

    update all request/

--- common/models/contract.js
+++ common/models/contract.js
@@ -6,26 +6,30 @@ const async = require('async');
 const contract_process = require("./../logic/contract_process");
 const pre = require("./../logic/preS3");
 const logTag = "> basemap.js model";
-module.exports = function (contract) {
+const result_bool = {
+  acknowledged: true
+};
+module.exports = function (Contrax) {
+  Contrax.disableRemoteMethodByName('create');
+  Contrax.disableRemoteMethodByName('upsert');
+  Contrax.disableRemoteMethodByName("updateAll");
+  Contrax.disableRemoteMethodByName("deleteById");
+  Contrax.disableRemoteMethodByName("updateAttributes");
+  Contrax.disableRemoteMethodByName("createChangeStream");
+  Contrax.disableRemoteMethodByName("patchOrCreate");
+  Contrax.disableRemoteMethodByName("replaceOrCreate");
+  Contrax.disableRemoteMethodByName("replaceById");
+  Contrax.disableRemoteMethodByName("upsertWithWhere");
 
-  contract.disableRemoteMethodByName('upsert');
-  contract.disableRemoteMethodByName("deleteById");
-  contract.disableRemoteMethodByName("updateAll");
-  //contract.disableRemoteMethodByName("updateAttributes");
-  contract.disableRemoteMethodByName("createChangeStream");
-
-  //contract.disableRemoteMethodByName("patchOrCreate");
-  contract.disableRemoteMethodByName("replaceOrCreate");
-  contract.disableRemoteMethodByName("replaceById");
-  contract.disableRemoteMethodByName("upsertWithWhere");
-
-  contract.observe('after save', function (ctx, next) {
+  Contrax.observe('after save', function (ctx, next) {
     if (ctx.instance) {
       const id = ctx.instance.userId;
-      ctx.instance.updateAttribute("userId", pre.makeId(contract, id), function (callback) {
-        console.log('update after save');
-        next();
-      });
+      /* ctx.instance.updateAttribute("userId", pre.makeId(Contrax, id), function (callback) {
+       console.log('update after save');
+       next();
+       });*/
+      console.log('update after save');
+      next();
     } else {
       next();
     }
@@ -35,20 +39,20 @@ module.exports = function (contract) {
    * @param user_id the user id
    * @param cb the call back
    */
-  contract.by_user = function (user_id, cb) {
-    contract_process.list_contracts(contract, user_id, cb);
+  Contrax.by_user = function (user_id, cb) {
+    contract_process.list_contracts(Contrax, user_id, cb);
   };
   /**
    * to confirm if the item is ready for sale
    * @param user_id user id
    * @param cb call back
    */
-  contract.sell_ready = function (user_id, cb) {
-    contract_process.approved_can_sell_now(contract, user_id, cb);
+  Contrax.sell_ready = function (user_id, cb) {
+    contract_process.approved_can_sell_now(Contrax, user_id, cb);
   };
 
-  contract.licensed = function (user_id, cb) {
-    contract.find({where: {userId: user_id}, limit: 20, order: "createtime DESC"}, (err, list) => {
+  Contrax.licensed = function (user_id, cb) {
+    Contrax.find({where: {userId: user_id}, limit: 20, order: "createtime DESC"}, (err, list) => {
       if (err) {
         cb(err, null);
         return;
@@ -70,8 +74,28 @@ module.exports = function (contract) {
       }
     });
   };
-
-  contract.remoteMethod("by_user", {
+  /**
+   *
+   * @param cid Contrax id
+   * @param b request body
+   * @param cb callback
+   */
+  Contrax.admin_action = function (cid, b, cb) {
+    Contrax.findById(cid, {}, function (err, oneDoc) {
+      if (err) cb(err, null);
+      // console.log("update what now", oneDoc, b.actionCode);
+      if (b.actionCode) {
+        oneDoc.updateAttribute("status", b.actionCode, function (err, r) {
+          if (err) cb(err, null);
+          //   console.log("update success", r);
+          cb(null, result_bool);
+        });
+      } else {
+        cb("cannot complete this request");
+      }
+    })
+  };
+  Contrax.remoteMethod("by_user", {
     description: ["Construct the certificate from given ids."],
     accepts: [
       {
@@ -88,7 +112,7 @@ module.exports = function (contract) {
     http: {verb: "get", path: "/by_user/:user_id"}
   });
 
-  contract.remoteMethod("licensed", {
+  Contrax.remoteMethod("licensed", {
     description: ["Request of query for a user having the license to issue the price or not."],
     accepts: [
       {
@@ -105,7 +129,7 @@ module.exports = function (contract) {
     http: {verb: "get", path: "/licensed/:user_id"}
   });
 
-  contract.remoteMethod("sell_ready", {
+  Contrax.remoteMethod("sell_ready", {
     description: ["Answer whether this user can make sell of artworks. depreciated as of 2017/3"],
     accepts: [
       {
@@ -122,29 +146,29 @@ module.exports = function (contract) {
     http: {verb: "get", path: "/sell_ready/:user_id"}
   });
 
-  /*contract.remoteMethod("construct_contract", {
-   description: ["Construct the certificate from given ids."],
-   accepts: [
-   {
-   arg: "user_id",
-   type: "string",
-   http: {source: "path"},
-   required: true,
-   description: "the user id gain contract certificate"
-   },
-   {
-   arg: "data", type: "object", http: {source: "body"},
-   required: true,
-   description: "when the admin resolve an issue"
-   },
-   {arg: 'req', type: 'object', 'http': {source: 'req'}},
-   {arg: 'res', type: 'object', 'http': {source: 'res'}}
-   ],
-   returns: {
-   arg: "luckylist", type: "object", root: true, description: "Return value"
-   },
-   http: {verb: "post", path: "/construct_contract/:user_id"}
-   });*/
+  Contrax.remoteMethod("admin_action", {
+    description: ["Construct the certificate from given ids."],
+    accepts: [
+      {
+        arg: "contractId",
+        type: "string",
+        http: {source: "path"},
+        required: true,
+        description: "Id of the Contrax pending certificate"
+      },
+      {
+        arg: "data", type: "object", http: {source: "body"},
+        required: true,
+        description: "what to do in admin action"
+      },
+      // {arg: 'req', type: 'object', 'http': {source: 'req'}},
+      // {arg: 'res', type: 'object', 'http': {source: 'res'}}
+    ],
+    returns: {
+      arg: "actioncomplete", type: "object", root: true, description: "Return value"
+    },
+    http: {verb: "post", path: "/action/:contractId"}
+  });
 
   /**
    * document
